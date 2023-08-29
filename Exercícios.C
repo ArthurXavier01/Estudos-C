40a06ee494ba4f1c4a6a5d08503c5ba10184c6cf
Arthur Xavier Cavalcante <129786001+ArthurXavier01@users.noreply.github.com>
2023-08-29 10:20:43 -0300

    Update Exercícios.C

--- Exercícios.C
+++ Exercícios.C
@@ -41,3 +41,74 @@ int main()
 	getchar();
 	getchar();
 }
+
+
+
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+
+{
+	setlocale(0,"Portuguese");
+	
+	
+// Primeiro Exercicio
+	
+	float soma;
+	float x, y;
+
+	printf("Digite as notas: ");
+	scanf("%f %f", &x, &y);
+	
+	soma = x + y;
+	
+	if(soma >= 6){
+		printf("Aluno Aprovado");
+	}
+	else if(soma >= 1 && soma < 6){
+		printf("Aluno está de Final");
+	}
+	else{
+		printf("Reprovado horroroso");
+	}
+
+
+// Segundo Exercicio	
+	
+	int numero;
+	float resultado;
+	
+	printf("Digite um numero inteiro: ");
+	scanf("%d", &numero);
+	
+	resultado = numero % 2;
+	
+	if(resultado == 0 )
+		printf("O numero lido foi %d e é par", numero);
+	else
+		printf("O numero lido é %d e é impar", numero);
+
+// Terceiro exercicio
+
+
+	int idade;
+
+	printf("Digite o ano de nascimento do nadador para avaliação: ");
+	scanf("%d", &idade);
+	
+	if( idade >= 5 && idade <= 7)
+		printf("Infantil A, com idade:   %d", idade);
+	else if( idade >= 8 && idade <= 10)
+		printf("Infantil B, com idade:   %d", idade);
+	else if (idade >= 11 && idade <= 13)
+		printf("Juvenil A, com idade:  %d", idade);
+	else if (idade >= 14 && idade <= 17)
+		printf("Juvenil B, com idade:  %d", idade);
+	else if (idade <5)
+		printf("não esta apto, com idade:  %d ", idade);
+	else
+		printf("Senior, com idade: %d ", idade);
+	
+	
+}
